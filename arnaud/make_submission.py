d8cc2a9ab772f0972e4c64b69635d882e260e363
ArnaudTeinturier <arnaud.teinturier@polytechnique.edu>
2023-11-19 14:58:33 +0100

    arnaud rf last commit

--- arnaud/make_submission.py
+++ arnaud/make_submission.py
@@ -6,7 +6,7 @@ import json
 from pathlib import Path
 
 
-def make_submission(json_path: Path = Path("arnaud/test_labels_text_rf.json")):
+def make_submission(json_path: Path = Path("arnaud/test_labels_text_METTRELEBONNOM.json")):
     with open(json_path, "r") as file:
         test_labels = json.load(file)
 
